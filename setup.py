4d3dab2efb692a10fb1c06e192192686607b5c32
Pascal Vizeli <pvizeli@syshack.ch>
2023-05-15 12:22:31 +0200

    Pump 2023.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "2023.01.0"
+VERSION = "2023.5.0"
 
 
 setup(
